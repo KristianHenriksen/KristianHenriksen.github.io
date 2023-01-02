2544605d6da57a37fbb6f29a06b51f625605c38d
Kristian Henriksen <kristianhenriksen@Kristians-MacBook-Pro-3.local>
2023-01-02 15:28:10 +0100

    added app.py

---
+++ static/script.js
@@ -0,0 +1,18 @@
+
+async function rotate(div){
+    console.log("test")
+    div.classList.add("rotate");
+    var content = document.getElementById("content").innerHTML
+    document.getElementById("content").innerHTML = "this feature is not yet implemented..."
+    await new Promise(r => setTimeout(r, 1000));
+    div.classList.remove("rotate");
+    document.getElementById("content").innerHTML = content
+
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function log(){
+    console.log("click")
+}
+
