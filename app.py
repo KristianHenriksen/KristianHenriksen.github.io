639230e506f85d8ae36c6e9aecf1e5d37a1530d6
Kristian Henriksen <kristianhenriksen@Kristians-MacBook-Pro-3.local>
2023-01-12 11:14:37 +0100

    trying to implement frozen-flask

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
-@app.route('/')
+@app.route("/")
 def index():
-  return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == '__main__':
-  app.run(port=8000, debug=True)
+    freezer.freeze()
