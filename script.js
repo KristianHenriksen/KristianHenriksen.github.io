24b749d87a0b0a8a20ff3bb79aa7b5ee98b7fab8
Kristian Henriksen <kristianhenriksen@Kristians-MacBook-Pro-3.local>
2023-01-12 18:42:01 +0100

    made some small changes

--- script.js
+++ script.js
@@ -2,11 +2,8 @@
 async function rotate(div){
     console.log("test")
     div.classList.add("rotate");
-    var content = document.getElementById("content1").innerHTML
-    document.getElementById("content1").innerHTML = "this feature is not yet implemented..."
     await new Promise(r => setTimeout(r, 1000));
     div.classList.remove("rotate");
-    document.getElementById("content1").innerHTML = content
 
 }
 function sleep(ms) {
